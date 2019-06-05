b5d1696d29a60fd33badbc8dff9f3f116bbbc359
Taiga KIYOKAWA <taiga.k127@gmail.com>
2019-06-06 08:14:11 +0900

    create controllers

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,8 @@ class Post < ApplicationRecord
   belongs_to :user
 
   validates :body, presence: true, length: {maximum: 120}
+
+  def user
+    return User.find_by(id: self.user_id)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,5 @@ class User < ApplicationRecord
 
   has_secure_password
   validates :name, presence: true, uniqueness: true, length: {maximum: 20}
+  validates :password, presence: true, length: {minimum: 6}
 end
