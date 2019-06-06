9e842099605fbc9d179e591aa87ff1d991f74e33
Taiga KIYOKAWA <taiga.k127@gmail.com>
2019-06-06 09:03:00 +0900

    install bootstrap

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,25 @@
 class UsersController < ApplicationController
+
+  def index
+  end
+
+  def show
+    @user = User.find_by(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
 end
